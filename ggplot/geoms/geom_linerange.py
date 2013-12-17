1225554e9d427c2412d9a7c75da96177cf0efd4c
Robert Gibboni <robertg@berkeley.edu>
2013-12-16 17:24:35 -0800

    reworked geom_linerange, accepts correct aes arguments, made conversion from ggplot2- to matplotlib-style yerr more apparent

--- ggplot/geoms/geom_linerange.py
+++ ggplot/geoms/geom_linerange.py
@@ -7,7 +7,7 @@ from .geom import geom
 from numpy import asarray
 
 class geom_linerange(geom):
-    VALID_AES = ['x', 'ymin', 'ymax', 'color', 'alpha', 'group', 'linestyle', 'linewidth' 'label', 'size']
+    VALID_AES = ['x', 'ymin', 'ymax', 'color', 'alpha', 'group', 'size']
     def plot_layer(self, layer):
         # select out valid arguments for this geom
         layer = {k: v for k, v in layer.items() if k in self.VALID_AES}
@@ -16,9 +16,10 @@ class geom_linerange(geom):
             x = layer.pop('x')
         ymin = asarray(layer.pop('ymin'))
         ymax = asarray(layer.pop('ymax'))
+        # reformat ggplot-style errors (specify absolute location of ymin and
+        # ymax) to matplotlib-style errors (specify error relative to y value)
         y = (ymax+ymin) / 2
-        ymin = y-ymin
-        ymax = ymax-y
+        yerr = abs(y-ymin)
         if 'size' in layer:
             # ggplot also supports aes(size=...) but the current mathplotlib is not. See 
             # https://github.com/matplotlib/matplotlib/issues/2658
@@ -30,12 +31,10 @@ class geom_linerange(geom):
                 sys.stderr.write(msg)
             layer['linewidth'] = layer['size']
             del layer['size']
-        if 'linestyle' in layer and 'color' not in layer:
-            layer['color'] = 'k'
         if 'group' not in layer:
-            plt.errorbar(x, y, yerr=[ymin, ymax], fmt=' ', capsize=0, **layer)
+            plt.errorbar(x, y, yerr=yerr, fmt=' ', capsize=0, **layer)
         else:
             g = layer.pop('group')
-            for k, v in groupby(sorted(zip(x, y, ymin, ymax, g), key=itemgetter(4)), key=itemgetter(4)):
-                x_g, y_g, ymin_g, ymax_g, _ = zip(*v)
-                plt.errorbar(x_g, y_g, yerr=[ymin_g, ymax_g], fmt=' ', capsize=0, **layer)
+            for k, v in groupby(sorted(zip(x, y, yerr, g), key=itemgetter(3)), key=itemgetter(3)):
+                x_g, y_g, yerr_g, _ = zip(*v)
+                plt.errorbar(x_g, y_g, yerr=yerr_g, fmt=' ', capsize=0, **layer)
